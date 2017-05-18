b8d65f0a00cd4991a13d65ec8b44bc9bbd37bb26
Gaetano Causio <gaetano.causio@gmail.com>
2017-05-18 23:15:16 +0200

    Watson API Playground: work in progress

--- IBMWatsonPlayground/ViewController.swift
+++ IBMWatsonPlayground/ViewController.swift
@@ -10,8 +10,46 @@ import UIKit
 import ToneAnalyzerV3
 
 class ViewController: UIViewController {
+    
+    func failure(error: Error) {
+        print("ERROR: \(error)")
+    }
 
+    override func viewDidLoad() {
+    
+        // just a quick test to check everything locally and from github is correctly working
+        // before I actually starting working on this this project
+        
+        let text = "I know the times are difficult! Our sales have been disappointing for " +
+            "the past three quarters for our data analytics product suite. We have a " +
+            "competitive data analytics product suite in the industry. But we need " +
+        "to do our job selling it! "
 
+        let toneAnalyzer = ToneAnalyzer(username: Credentials.toneAnalyzer.username!,
+                                    password: Credentials.toneAnalyzer.password!,
+                                    version: Credentials.toneAnalyzer.version!)
+        
+        toneAnalyzer.getTone(ofText: text, failure: failure) { tones in
+            
+            // find the most top tone scores per category
+            for value in tones.documentTone {
+                var winnerScore = 0.0
+                var winnerName = ""
+                for tone in value.tones {
+                    if tone.score > winnerScore {
+                        winnerScore = tone.score
+                        winnerName = tone.name
+                    }
+                }
+                if let firstWord = value.name.components(separatedBy: " ").first {
+                    print("\(firstWord): \(winnerName) (\(Int(winnerScore*100))%)")
+                }
+            }
+            
+
+        }
+        
+    }
 
 }
 
