6dd096ee688710e0fbef4443feffd9c0b3571c26
Gaetano Causio <gaetano.causio@gmail.com>
2017-05-24 09:56:32 +0200

    extend watson api with JSONEncodable

--- Models/ToneAnalysisModel.swift
+++ Models/ToneAnalysisModel.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import ToneAnalyzerV3
+import RestKit
 
 // MARK: - Public Interface
 
@@ -39,3 +40,59 @@ fileprivate struct ToneAnalysisModelHelper {
 
     
 }
+
+// MARK: - Watson Model Extensions
+
+// Make the following objects JSONEncodable so we can get a Json string from it (for mockup)
+
+extension ToneScore: JSONEncodable {
+    
+    /// Used to serialize this model to JSON.
+    public func toJSONObject() -> Any {
+        var json = [String: Any]()
+        json["tone_id"] = id
+        json["tone_name"] = name
+        json["score"] = score
+        return json
+    }
+}
+
+extension ToneCategory: JSONEncodable {
+    
+    /// Used to serialize this model to JSON.
+    public func toJSONObject() -> Any {
+        var json = [String: Any]()
+        json["category_name"] = name
+        json["category_id"] = categoryID
+        json["tones"] = tones.map { $0.toJSONObject() }
+        return json
+    }
+}
+
+extension SentenceAnalysis: JSONEncodable {
+    
+    /// Used to serialize this model to JSON.
+    public func toJSONObject() -> Any {
+        var json = [String: Any]()
+        json["sentence_id"] = sentenceID
+        json["input_from"] = inputFrom
+        json["input_to"] = inputTo
+        json["text"] = text
+        json["tone_categories"] = toneCategories.map { $0.toJSONObject() }
+        return json
+    }
+}
+
+extension ToneAnalysis: JSONEncodable {
+    
+    /// Used to serialize this model to JSON.
+    public func toJSONObject() -> Any {
+        var json = [String: Any]()
+        json["documentTone"] = documentTone.map { $0.toJSONObject() }
+        if let sentencesTones=sentencesTones {
+            json["sentencesTones"] = sentencesTones.map { $0.toJSONObject() }
+        }
+        return json
+    }
+}
+
